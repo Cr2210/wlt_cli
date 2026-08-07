33b4d1044463b2da7110a63481f18307271096af
PENGGE <2580819326@qq.com>
2026-08-07 15:23:43 +0800

    refactor(purchase,sale/return): 迁移到 DocumentCmds，删除 crud_legacy.go

--- cmd/sale/sale_return.go
+++ cmd/sale/sale_return.go
@@ -3,5 +3,20 @@ package sale
 import "github.com/weiliantong/cli/internal/cmdutil"
 
 func init() {
-	saleCmd.AddCommand(cmdutil.NewCRUDSubCmd("return", "/erp/sale-return", "销售退货"))
+	cmdutil.DocumentCmds(saleCmd,
+		cmdutil.DocumentConfig{
+			Name:    "return",
+			APIPath: "/erp/sale-return",
+			Label:   "销售退货",
+			Filters: []cmdutil.FlagSpec{
+				{Name: "warehouse-id", Usage: "仓库 ID"},
+				{Name: "product-id", Usage: "产品 ID"},
+				{Name: "no", Usage: "单号"},
+				{Name: "status", Usage: "状态"},
+				{Name: "start-time", Usage: "开始时间"},
+				{Name: "end-time", Usage: "结束时间"},
+				{Name: "type", Usage: "类型"},
+			},
+		},
+	)
 }
